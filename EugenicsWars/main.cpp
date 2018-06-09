7c72dc0cb595f0c464a369798e5902e8c8cf5c1a
SirEmentaler <sir.ementaler@gmail.com>
2018-06-09 20:15:12 +0200

    Add a point mutator

--- EugenicsWars/main.cpp
+++ EugenicsWars/main.cpp
@@ -28,9 +28,25 @@ private:
 	std::uniform_real_distribution<> range_y {0.0, 15.0};
 };
 
-double f(const point& point) noexcept {
-	const double a = point.x * point.x - point.y;
-	const double b = 1 - point.x;
+template<class UniformRandomBitGenerator>
+class point_mutator {
+public:
+	point_mutator(const UniformRandomBitGenerator& g)
+		: rand(g) {}
+	point operator()(const point& p) {
+		point result = p;
+		result.x += distribution(rand);
+		result.y += distribution(rand);
+		return result;
+	}
+private:
+	std::normal_distribution<double> distribution;
+	UniformRandomBitGenerator rand;
+};
+
+double f(const point& p) noexcept {
+	const double a = p.x * p.x - p.y;
+	const double b = 1 - p.x;
 	return a * a * 100.0 + b * b + 10.0;
 }
 
@@ -47,12 +63,12 @@ int main() {
 	using algorithm_type = genetic_algorithm<point, double>;
 	algorithm_type::context_type context;
 	context.initial_population_size = 10000;
-	context.breeding_population_size = 200;
+	context.breeding_population_size = 100;
 	context.max_iterations = 10;
 	context.generator = point_generator(rand);
 	context.evaluator = f;
 	context.selector = roulette_wheel_selection(rand, &negative_exponent);
-	context.breeder = mutating_breeder(&average, mutate_with_probability(rand, 0.5, identity()));
+	context.breeder = mutating_breeder(&average, mutate_with_probability(rand, 0.25, point_mutator(rand)));
 	context.comparator = std::greater<>();
 	algorithm_type algorithm(context);
 	repeat(10, [&] {
